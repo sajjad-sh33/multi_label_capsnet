3bac69524ba7c2ede214fed7b6bf2d3611b14da1
sajjad-sh33 <79309701+sajjad-sh33@users.noreply.github.com>
2023-02-26 11:56:13 +0330

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -11,9 +11,9 @@ class Dataset:
                 transforms.Normalize((0.1307,), (0.3081,))
             ])
 
-            train_dataset = datasets.MNIST('/data/mnist', train=True, download=False,
+            train_dataset = datasets.MNIST('/data/mnist', train=True, download=True,
                                            transform=dataset_transform)
-            test_dataset = datasets.MNIST('/data/mnist', train=False, download=False,
+            test_dataset = datasets.MNIST('/data/mnist', train=False, download=True,
                                           transform=dataset_transform)
 
             self.train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=_batch_size, shuffle=True)
